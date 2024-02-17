e3b4a7b4e18c837080fa1753917a8ad3d0470d2e
Sarthak0085 <sarth.mahajan@gmail.com>
2024-02-17 21:12:42 +0530

    responsiveness added

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
       <div className='bg-[#121121] pt-[4rem] pb-[4rem] md:pt-[4rem]'>
            <h1 className='heading pb-[4rem]'>About <span className='text-orange-400'>Me</span></h1>
-          <div data-aos="fade-right" data-aos-delay="300" className='grid grid-cols-1 1150:grid-cols-2 w-[80%] mx-auto gap-[3rem] lg:justify-start justify-center items-center'>
+          <div data-aos="fade-right" data-aos-delay="300" className='grid grid-cols-1 1150:grid-cols-2 w-[80%] 1150:w-[90%] mx-auto gap-[3rem] lg:justify-start justify-center items-center'>
               <div className='flex items-center mx-auto 1150:mx-0 justify-center 1150:justify-start flex-col'>
                   <h2 className='text-[25px] md:text-[35px] 1150:text-[45px] leading-[2rem] md:leading-[3rem] capitalize 
                   mb-[3rem] font-bold text-white'>
@@ -16,7 +16,7 @@ const About = () => {
                   </h2>
                   <div className='mb-[3rem] flex justify-center 1150:justify-start items-center md:space-x-10'>
                   <span className='w-[100px] hidden md:block bg-gray-400 h-[5px] rounded-md'></span>
-                  <p className='text-[20px] text-slate-300 w-[80%]'>
+                  <p className='text-[20px] text-slate-300 w-[80%] 1150:w-[90%]'>
                         I&apos;m a self-taught coder with a background in textiles, now deeply passionate about coding.
                         From textiles to coding, I&apos;ve embraced learning new skills with enthusiasm.
                         I love the challenge of coding and enjoy creating solutions that make a difference. Now, I want to apply
@@ -28,8 +28,8 @@ const About = () => {
                       <a href='images/Sarthak_Resume.pdf'
                           download="Sarthak_Resume.pdf"
                           target='_blank'
-                          className='px-[2rem] hover:bg-orange-400 transition-all duration-200 py-[1rem] text-[20px] font-bold
-                              flex  text-black bg-blue-200 space-x-2'>
+                          className='px-[1.5rem] hover:bg-orange-400 transition-all duration-200 py-[1rem] text-[20px] font-bold
+                              flex text-black bg-blue-200 gap-2'>
                            Download CV
                           <FiDownload className="h-[1.7rem] w-[1.6rem] text-black" />
                       </a>
